93bce841334067cc5f23ec7c2dd13269241b2fdc
Stephan Fuhrmann <s_fuhrm@gmx.de>
2014-11-14 00:43:23 +0100

    Unnecessary import

--- src/main/java/de/tynne/schrumpf/ui/MainFrame.java
+++ src/main/java/de/tynne/schrumpf/ui/MainFrame.java
@@ -9,7 +9,6 @@ import de.tynne.schrumpf.prefs.BeanPrefsMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
